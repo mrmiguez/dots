46b4af2d3a17f86c0c7793a0d232de56b5331ad8
Matthew Miguez <mmiguez@pop-os.localdomain>
2023-01-11 09:04:28 -0800

    some updates

--- start-up.sh
+++ start-up.sh
@@ -14,7 +14,7 @@
 
 # Update and install
 sudo apt update && sudo apt upgrade -y
-sudo apt install vim xmlstartlet libxml2-utils zsh tmux fonts-powerline
+sudo apt install vim xmlstartlet libxml2-utils zsh tmux fonts-powerline -y
 
 # Clone and place dot files
 mkdir ~/bin
@@ -30,13 +30,13 @@ ln -s ~/.tmux/mrmiguez.tmux ~/.tmux.conf
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ~/bin/dots/.zshrc ~/.zshrc
-cp -r ~/bin/dots/.oh-my-zsh/custom/ ~/.oh-my-zsh/custom
-cp -r ~/bin/dots/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
-ln -s ~/.oh-my-zsh/custom/themes/gruvbox-zsh/gruvbox.zsh-theme ~/.oh-my-zsh/custom/themes
+cp ~/bin/dots/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
+cp ~/bin/dots/gruvbox.zsh-theme ~/.oh-my-zsh/custom/themes
 source ~/.zshrc
 
+# saxon HE 11.4
 mkdir -p ~/.local/share/saxon
-#curl https://sourceforge.net/projects/saxon/files/Saxon-HE/11/Java/SaxonHE11-4J.zip/download -o ~/.local/share/saxon/saxonHE11-4J.zip
+cp -r ~/bin/dots/saxon/* ~/.local/share/saxon
 
 # AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -49,3 +49,7 @@ rm awscliv2.zip
 curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
 sudo dpkg -i session-manager-plugin.deb
 rm session-manager-plugin.deb
+
+# MODS schema
+mkdir ~/.local/share/schema
+cp ~/bin/dots/*.xsd ~/.local/share/schema
